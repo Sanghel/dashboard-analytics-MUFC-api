75cc3ab7348b7b493d889acaacc3f124c3460114
Sanghel Gonzalez <98786745+Sanghel@users.noreply.github.com>
2026-03-27 16:39:43 -0500

    feat: implement API-Football fetchers for fixtures, standings, players and team stats (#21) (#22)

--- src/jobs/fetchers/fetchPlayerStats.js
+++ src/jobs/fetchers/fetchPlayerStats.js
@@ -1,1 +1,35 @@
-// TODO: Fetch player stats from API Sports
+const apiClient = require('../../utils/apiClient');
+const { withRateLimit } = require('../../utils/rateLimitGuard');
+const { transformPlayer } = require('../../utils/transformers');
+const { upsertPlayer } = require('../../models/players.model');
+
+const TEAM_ID = parseInt(process.env.MUFC_TEAM_ID || '33', 10);
+const SEASON = parseInt(process.env.CURRENT_SEASON || '2024', 10);
+
+const fetchPlayerStats = async () => {
+  let page = 1;
+  let totalPages = 1;
+  let totalSaved = 0;
+
+  do {
+    const response = await withRateLimit(() =>
+      apiClient.get('/players', {
+        params: { team: TEAM_ID, season: SEASON, page },
+      })
+    );
+
+    const players = response.data.response || [];
+    totalPages = response.data.paging?.total || 1;
+
+    for (const p of players) {
+      await upsertPlayer(transformPlayer(p, SEASON, TEAM_ID));
+    }
+
+    totalSaved += players.length;
+    page++;
+  } while (page <= totalPages);
+
+  return totalSaved;
+};
+
+module.exports = { fetchPlayerStats };
