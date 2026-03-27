8748bd6de995e1a1e6ef05c916690930833cf301
Sanghel Gonzalez <98786745+Sanghel@users.noreply.github.com>
2026-03-27 16:46:11 -0500

    feat: implement all controllers, routes and wire up router (#28) (#29)

--- src/routes/players.routes.js
+++ src/routes/players.routes.js
@@ -1,1 +1,7 @@
-// TODO: Players routes
+const router = require('express').Router();
+const { list, detail } = require('../controllers/players.controller');
+
+router.get('/', list);
+router.get('/:id', detail);
+
+module.exports = router;
--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -1,1 +1,6 @@
-// TODO: Team routes
+const router = require('express').Router();
+const { stats } = require('../controllers/team.controller');
+
+router.get('/stats', stats);
+
+module.exports = router;
